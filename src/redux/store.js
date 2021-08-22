24535d2f7ba85d48f96469a8338289d00eebc42a
minkuppj <63226917+minkuppj@users.noreply.github.com>
2021-08-22 23:27:26 +0700

    update

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import todoReducers from './todoSlice';
+
+
+export default configureStore({
+    reducer:{
+        todos: todoReducers,
+        
+    }
+});
